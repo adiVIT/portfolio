f60ab73b8cb4c401ab36fc8d3569300609623c25
adiVIT <aditya.bajaj@epifi.com>
2025-06-22 19:08:49 +0530

    Fix Vercel build issue - remove corrupted yarn.lock and clean up unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useEffect } from 'react'
 import { Banner } from './components/Banner/Banner'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
